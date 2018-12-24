f15426e8e62ee4e91b22370ecc9e09b548738ef3
msuzuki <led.centipede@gmail.com>
2018-12-25 02:07:37 +0900

    Libra検査結果確認用画面ｰ詳細ページ表をExcelに変換機能完成
    tr = html.unescape(lxml.html.tostring(row).decode())
    では、tableタグ等の実体参照で不具合発生するため、下記の修正が必要だった（これに気づくのに大苦労した）
    tr = lxml.html.tostring(row).decode()

--- scraping/libra_report_bots.py
+++ scraping/libra_report_bots.py
@@ -7,6 +7,8 @@ import yaml
 import lxml.html
 import html
 import re
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side, Font, Alignment
 
 class LibraRepBots:
     def __init__(self, projectID):
@@ -18,10 +20,12 @@ class LibraRepBots:
             self.longWait = userdata["longWait"]
             self.midWait = userdata["midWait"]
             self.shortWait = userdata["shortWait"]
+            self.hrefFlag = userdata["hrefFlag"]
         self.app_url = "https://accessibility.jp/libra/"
         self.index_url = "https://jis.infocreate.co.jp/"
         self.rep_index_url_base = "http://jis.infocreate.co.jp/diagnose/indexv2/report/projID/"
         self.rep_detail_url_base = "http://jis.infocreate.co.jp/diagnose/indexv2/report2/projID/"
+        self.sv_mainpage_url_base = "http://jis.infocreate.co.jp/diagnose/indexv2/index/projID/"
         self.guideline_filename = "guideline_datas.txt"
         self.projectID = projectID
         self.wd = webdriver.PhantomJS()
@@ -61,6 +65,9 @@ class LibraRepBots:
     def fetch_report_detail_path(self, pageID, guidelineID):
         return self.rep_detail_url_base + self.projectID + "/controlID/" + pageID + "/guideline/" + guidelineID + "/"
 
+    def fetch_sv_mainpage_path(self, pageID):
+        return self.sv_mainpage_url_base + self.projectID + "/controlID/" + '"' + pageID + '"'
+
     def get_dom(self):
         html_str = self.wd.page_source
         return lxml.html.fromstring(html_str)
@@ -74,7 +81,7 @@ class LibraRepBots:
             datas.append(td_val)
         return datas
     
-    def get_detail_table_data(self, pageID):
+    def get_detail_table_data(self, pageID, guideline):
         datas = []
         dom = self.get_dom()
         tbl = dom.xpath("/html/body/div[2]/div[2]/table")[0]
@@ -84,25 +91,30 @@ class LibraRepBots:
             if cnt < 2:
                 pass
             else:
-                tr = html.unescape(lxml.html.tostring(row).decode())
+                tr = lxml.html.tostring(row).decode()
                 trdom = lxml.html.fromstring(tr)
-                row_text = pageID + ","
+                row_datas = []
+                row_datas.append(pageID)
+                row_datas.append(guideline)
+                if self.hrefFlag == "yes":
+                    row_datas.append(self.fetch_sv_mainpage_path(pageID))
+                else:
+                    pass
                 for irow in trdom.cssselect("td"):
-                    td_val = irow.text
+                    td_val = irow.text_content()
                     if td_val is None:
                         if self.is_including_tag(irow) is True:
-                            row_text += self.get_regx_text(irow) + ","
+                            row_datas.append(self.get_regx_text(irow))
                         else:
-                            row_text += ","
+                            row_datas.append("")
                     else:
-                        row_text += td_val.strip() + ","
-                print(row_text)
-                datas.append(row_text)
+                        row_datas.append(td_val.strip())
+                datas.append(row_datas)
         return datas
-    
+
     def is_including_tag(self, elm):
         tag_str = html.unescape(lxml.html.tostring(elm).decode())
-        if re.search(r'<td.*?>(.+?)</td>', tag_str.strip(), re.DOTALL):
+        if re.search(r'<td.*?>(.+)</td>', tag_str.strip(), re.DOTALL):
             return True
         else:
             return False
@@ -110,7 +122,7 @@ class LibraRepBots:
     def get_regx_text(self, elm):
         tag_str = html.unescape(lxml.html.tostring(elm).decode())
         tag_str = re.sub(r'(\r|\n|\r\n|\t|\s{2,})', "", tag_str)
-        return re.search(r'<td.*?>(.+\!*?)</td>', tag_str.strip(), re.DOTALL).group(1)
+        return re.search(r'<td.*?>(.+)</td>', tag_str.strip(), re.DOTALL).group(1)
 
     def open_text_data(self, filename):
         line = []
@@ -121,14 +133,60 @@ class LibraRepBots:
     def save_text(self, text_data):
         with open(self.fetch_filename_from_datetime(".txt"), "w") as f:
             f.write(text_data)
+    
+    def save_xlsx(self, datas):
+        wb = Workbook()
+        ws = wb.active
+        border_style = Border(
+            left = Side(border_style="thin", color='FF000000'),
+            right = Side(border_style="thin", color='FF000000'),
+            top = Side(border_style="thin", color='FF000000'),
+            bottom = Side(border_style="thin", color='FF000000'),
+        )
+        r = 1
+        for parent_row in datas:
+            c = 1
+            for child_row in parent_row:
+                ws.cell(row=r, column=c).value = child_row
+                ws.cell(row=r, column=c).border = border_style
+                ws.cell(row=r, column=c).alignment = Alignment(vertical="top")
+                c += 1
+            cellobj = None
+            if self.hrefFlag == "yes":
+                cellobj = ws.cell(row=r, column=6)
+            else:
+                cellobj = ws.cell(row=r, column=5)
+            if cellobj.value == "適合":
+                for i in range(1, len(parent_row)+1):
+                    ws.cell(row=r, column=i).fill = PatternFill(fill_type="solid", fgColor="FF40FFFF")
+            elif cellobj.value == "適合(注記)":
+                for i in range(1, len(parent_row)+1):
+                    ws.cell(row=r, column=i).fill = PatternFill(fill_type="solid", fgColor="FF40FF40")
+            elif cellobj.value == "不適合":
+                for i in range(1, len(parent_row)+1):
+                    ws.cell(row=r, column=i).fill = PatternFill(fill_type="solid", fgColor="FFFF8080")
+            elif cellobj.value == "非適用":
+                for i in range(1, len(parent_row)+1):
+                    ws.cell(row=r, column=i).fill = PatternFill(fill_type="solid", fgColor="FFC0C0C0")
+            else:
+                pass
+            r += 1
+        rangeobj = None
+        if self.hrefFlag == "yes":
+            rangeobj = range(1, 11)
+        else:
+            rangeobj = range(1, 10)
+        for i in rangeobj:
+            ws.cell(row=1, column=i).font = Font(bold=True)
+            ws.cell(row=1, column=i).alignment = Alignment(horizontal="center")
+        wb.save(self.fetch_filename_from_datetime(".xlsx"))
 
 
 class LibraRepBotsUtil(LibraRepBots):
-
     def fetch_report_sequential(self, guideline_arr):
-        result_text = ""
+        rep_data = []
         self.wd.get(self.rep_index_url_base + self.projectID + "/")
-        if guideline_arr is None:
+        if guideline_arr is False:
             guideline_rows = self.open_text_data(self.guideline_filename)
         else:
             guideline_rows = guideline_arr
@@ -139,21 +197,31 @@ class LibraRepBotsUtil(LibraRepBots):
                 path = self.fetch_report_detail_path(pageID, guideline)
                 self.wd.get(path)
                 time.sleep(self.shortWait)
-                for line in self.get_detail_table_data(pageID):
-                    result_text += line + "\n"
-                #self.save_sc(pageID + "_" + guideline + ".png")
-        self.save_text(result_text)
+                rep_data.extend(self.get_detail_table_data(pageID, guideline))
+        add_header = None
+        if self.hrefFlag == "yes":
+            add_header = [["管理番号", "達成基準", "LibraURL", "状況/要件", "実装番号", "検査結果", "検査員", "コメント", "対象ソースコード", "修正ソースコード"]]
+        else:
+            add_header = [["管理番号", "達成基準", "状況/要件", "実装番号", "検査結果", "検査員", "コメント", "対象ソースコード", "修正ソースコード"]]
+        last_rep_data = add_header + rep_data
+        self.save_xlsx(last_rep_data)
 
-    
 
 args = sys.argv
 projectID = args[1]
-techID = args[2]
+techID = None
+try:
+    techID = args[2]
+except IndexError:
+    pass
 lbt = LibraRepBotsUtil(projectID)
 time.sleep(lbt.shortWait)
 lbt.login()
 time.sleep(lbt.shortWait)
-lbt.fetch_report_sequential([techID])
+if techID is None:
+    lbt.fetch_report_sequential(False)
+else:
+    lbt.fetch_report_sequential([techID])
 time.sleep(lbt.shortWait)
 lbt.logout
 time.sleep(lbt.shortWait)
