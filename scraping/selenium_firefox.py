d1091a4e8e158ff72cfc40a8af029d17b94e9774
Unknown <led.centipede@gmail.com>
2018-12-29 00:19:38 +0900

    class化のためのリファクタリング

--- scraping/selenium_firefox.py
+++ scraping/selenium_firefox.py
@@ -1,25 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.keys import Keys
 from subprocess import Popen, PIPE
 import time
+from os.path import expanduser
 
-options = Options()
-options.binary_location = '/usr/bin/firefox'
+user_dir = expanduser('~')
+
+fx_options = FirefoxOptions()
+fx_options.binary_location = '/usr/bin/firefox'
+
 # options.add_argument('-headless')
 # unix-command: cat "$HOME/.mozilla/firefox/profiles.ini" | sed -n -e 's/^.*Path=//p' | head -n 1
-profile_dir = '/home/vagrant/.mozilla/firefox/'
-profile_fname = Popen('cat "$HOME/.mozilla/firefox/profiles.ini" | sed -n -e \'s/^.*Path=//p\' | head -n 1', shell=True, stdout=PIPE).communicate()[0].decode().strip()
-profile_my_path = profile_dir + profile_fname
-options.profile = profile_my_path
-driver = webdriver.Firefox(firefox_options=options)
+fx_profile_dir = user_dir + '/.mozilla/firefox/'
+fx_profile_fname = Popen('cat "$HOME/.mozilla/firefox/profiles.ini" | sed -n -e \'s/^.*Path=//p\' | head -n 1', shell=True, stdout=PIPE).communicate()[0].decode().strip()
+fx_options.profile = fx_profile_dir + fx_profile_fname
+fx_wd = webdriver.Firefox(firefox_options=fx_options)
 
-driver.get('https://www.google.co.jp/')
-# elm = driver.find_element_by_tag_name("body")
+fx_wd.get('https://www.google.co.jp/')
+# elm = fx_wd.find_element_by_tag_name("body")
 # elm.send_keys(Keys.CONTROL, "a")
-html = driver.page_source
+html = fx_wd.page_source
 time.sleep(10)
-driver.save_screenshot("google.png")
+fx_wd.save_screenshot("google.png")
 print(html)
 
-driver.quit()
+fx_wd.quit()
