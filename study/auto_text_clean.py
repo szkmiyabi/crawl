dc2918c8173077fa8a254b224e3badfe76a366c3
msuzuki <led.centipede@gmail.com>
2019-01-06 15:14:02 +0900

    # bugfix

--- study/auto_text_clean.py
+++ study/auto_text_clean.py
@@ -3,17 +3,9 @@ import os
 import re
 from glob import glob
 
-for path in glob(os.path.join("text", "*.txt")):
-    with open(path, "r") as f:
-        tmpdata = f.read()
-    tmpdata = text_clean(tmpdata)
-    print(tmpdata)
-    with open(path, "w") as f2:
-        f.write(tmpdata)
-
 def text_clean(content):
-    pt1 = re.compile(r'^\s+', re.DOTALL)
-    pt2 = re.compile(r'^-+ Page.+', re.DOTALL)
+    pt1 = re.compile(r'^-+ Page.+', re.DOTALL)
+    pt2 = re.compile(r'^\s+', re.DOTALL)
     pt3 = re.compile(r' +$', re.DOTALL)
     pt4 = re.compile(r'\s{2,}', re.DOTALL)
     content = re.sub(pt1, "", content)
@@ -21,3 +13,12 @@ def text_clean(content):
     content = re.sub(pt3, "", content)
     content = re.sub(pt4, "", content)
     return content
+
+for path in glob(os.path.join("text", "*.txt")):
+    with open(path, "r") as f1:
+        tmpdata = f1.read()
+    tmpdata = text_clean(tmpdata)
+    print(tmpdata)
+    with open(path, "w") as f2:
+        f2.write(tmpdata)
+
