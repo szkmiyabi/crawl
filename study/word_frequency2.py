78065a20d1ccebae81c80163da2ec90dcf7e3f57
msuzuki <led.centipede@gmail.com>
2019-01-06 15:53:39 +0900

    #bug fix

--- study/word_frequency2.py
+++ study/word_frequency2.py
@@ -17,22 +17,14 @@ def main():
         print('Processing {0}...'.format(path), file=sys.stderr)
 
         with open(path) as file:
-            #for content in iter_docs(file):
             content = file.read()
-            content = text_clean(content)
             tokens = get_tokens(tagger, content)
             frequency.update(tokens)
             count_processed += 1
-            if count_processed % 1000 == 0:
-                print('{0} documents were processed.'.format(count_processed), file=sys.stderr)
+            print('{0} documents were processed.'.format(count_processed), file=sys.stderr)
     
     for token, count in frequency.most_common():
         print(token, count)
-
-def text_clean(content):
-    txt = None
-    txt = re.sub(r'(\t|\s{3,}|　{2,}|\r\n{2,}|\n{2,})+', "", content, re.DOTALL)
-    return txt
 
 def get_tokens(tagger, content):
     tokens = []
